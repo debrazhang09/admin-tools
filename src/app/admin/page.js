9b69309a36ffed7ff2bbe8598e39c73f6f081922
debrazhang <debrazhang09@gmail.com>
2025-08-04 11:33:25 -0700

    add header

---
+++ src/app/admin/page.js
@@ -0,0 +1,11 @@
+import AdminHeader from '../components/AdminHeader';
+export default function AdminPage() {
+  return (
+    <div className="max-w-[90rem]" >
+      <AdminHeader imageUrl="/dan_photo.jpg" name="Dan Zhang"/>
+    </div>
+
+  )
+
+
+}
---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+import AdminHeader from './components/AdminHeader';
+
+
+export default function Home() {
+  return (
+    <h1>Demo: Welcome to home page</h1>
+  );
+}
